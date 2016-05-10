6ee943102796793b799e631d6c05aee19c2dc132
William Heymann <w.heymann@fz-juelich.de>
2016-05-10 15:09:46 +0200

    Well this was an easy bug to fix. Browsing your own simulations was broken due to this file being missing

---
+++ CadetWeb/plugins/search/find_by_user.py
@@ -0,0 +1,37 @@
+__author__ = 'kosh_000'
+name = "Find by User"
+
+from simulation import models
+
+def run(request):
+    if request.user.is_superuser:
+        html_string = '''
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group">
+                  <div class="col-sm-2">
+                    <label for="user_name" class="control-label">User Name</label>
+                  </div>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control required" placeholder="" name="user_name" id="user_name">
+                  </div>
+            </div>
+          </div>
+          '''
+    else:
+        html_string = ''
+    return html_string
+
+def process_search(request, search_dict):
+    if request.user.is_superuser:
+        user_name = search_dict['user_name']
+    else:
+        user_name = request.user.username
+
+    results = models.Job.objects.filter(username=user_name)
+
+    headers = ('JOBID',)
+    search_results = []
+    for result in results:
+        search_results.append( (result.id, ))
+    return headers, search_results
