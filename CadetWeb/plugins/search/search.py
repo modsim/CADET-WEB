2e75e28ddeb535973caaad2f1369db8c41459c3b
William Heymann <w.heymann@fz-juelich.de>
2016-05-13 15:23:06 +0200

    The search feature partially works now.

--- CadetWeb/plugins/search/search.py
+++ CadetWeb/plugins/search/search.py
@@ -9,14 +9,90 @@ def run(request):
     html_string = '''
     <div class="row">
         <div class="col-md-12">
-           <p>Browse all simulations.</p>
+           <div class="form-group">
+              <div class="col-sm-2"><label class="control-label">Search Data Range</label></div>
+              <div class="col-sm-1">
+                <label for="lb" class="control-label">From</label>
+              </div>
+              <div class="col-sm-2 date">
+                <input type="date" class="form-control" name="lb" id="lb">
+              </div>
+              <div class="col-sm-1">
+                <label for="ub" class="control-label">To</label>
+              </div>
+              <div class="col-sm-2 date">
+                <input type="date" class="form-control" name="ub" id="ub">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-2">
+                <label for="username" class="control-label">Username</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="username" id="username">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-2">
+                <label for="study_name" class="control-label">Study Name</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="study_name" id="study_name">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-2">
+                <label for="product_name" class="control-label">Product Name</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="product_name" id="product_name">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-2">
+                <label for="model_name" class="control-label">Model Name</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="model_name" id="model_name">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-2">
+                <label for="notes" class="control-label">Notes</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="notes" id="notes">
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-sm-2">
+                <label for="isotherm" class="control-label">Isotherm</label>
+              </div>
+              <div class="col-sm-10">
+                <input type="text" class="form-control" name="isotherm" id="isotherm">
+              </div>
+            </div>
         </div>
       </div>
       '''
     return html_string
 
 def process_search(request, search_dict):
-    results = models.Job.objects.all()
+    lb = search_dict['lb']
+    ub = search_dict['ub']
+    username = search_dict['username']
+    study_name = search_dict['study_name']
+    product_name = search_dict['product_name']
+    model_name = search_dict['model_name']
+    notes = search_dict['notes']
+    isotherm = search_dict['isotherm']
+
+
+    # job_notes__notes__icontains = notes, 
+    results = models.Job.objects.filter(study_name__icontains = study_name,
+                                        username__icontains = username,
+                                        Model_ID__model__icontains = model_name,
+                                        Product_ID__product__icontains = product_name)
 
     headers = ('JOBID',)
     search_results = []
